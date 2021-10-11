86110adde2dfa81b1ba4416a722b0b18a65414f1
Anusha Nambiar <aan29@cornell.edu>
2021-10-11 19:48:35 -0400

    some progress with table

--- src/table.ml
+++ src/table.ml
@@ -1,7 +1,30 @@
-type table = Card.card list list
+open Card
+type table = card list list
 
 exception InvalidCombo
 
 exception NoSuchCard
 
 let check_valid (tb : table) = true
+type set = {
+  kind: set_type; 
+  cards: card list; 
+}
+
+let rec valid_group cards = 
+  match with 
+  | _ :: _ :: [] -> false
+  | f :: s :: t :: [] -> ((f.number = s.number) && (s.number = t.number)) &&
+    ((f.color <> s.color) && (s.color <> t.color) && (f.color <> t.color))
+  | f :: s :: tl -> if (f.number = s.number) && (f.color <> s.color) then 
+    true && valid_group s::tl else false
+
+let valid_run cards = 
+  match with 
+  | _ :: _ :: [] -> false
+  | f :: s :: t :: []-> if ((f.color = s.color) && (f.color = t.color)) then 
+    ((succ f.number = s.number) && (succ s.number = t.number))
+  | f :: s :: tl -> if (f.color = s.color) && (succ f.number = s.number) then 
+    valid_inc_run s :: tl else false
+
+let valid_set set = valid_run set || valid_group set.cards; 
--- src/table.mli
+++ src/table.mli
@@ -1,3 +1,10 @@
+
+
+(** Representation of dynamic gameplay.
+
+    This module contains the functions that involve playing a card, checking
+    set validity, ....
+**)
 type table
 
 exception InvalidCombo
@@ -5,3 +12,18 @@ exception InvalidCombo
 exception NoSuchCard
 
 val check_valid : table -> bool
+
+type set_type = 
+  | Run
+  | Group
+
+type card 
+(** The abstract type of values representing a card*)
+
+type set
+(** The abstract type representing a set*)
+
+
+val valid_set : set -> bool
+(** [valid_set s] return true if s is a valid run or group and false otherwise.
+   [s] is of type set. *)
