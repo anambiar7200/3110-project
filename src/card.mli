41d000f6815bb82874a15cc31514788e2b7d2745
Ja Young Byun <jb2297@cornell.edu>
2021-10-11 20:24:47 -0400

    added documentation

--- src/card.mli
+++ src/card.mli
@@ -1,10 +1,14 @@
 type color_type
+(** The type of color. *)
 
 type position_type
+(** The type of card position. *)
 
 type card
+(** The type of card. *)
 
 val create_card :
   int -> int -> color_type -> int -> card list -> card list
 
 val card_deck : card list
+(** card_deck is list of 104 rummikub cards*)
