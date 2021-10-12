b3111d02f294b7bf28d66a2eec8d9fa0f8ca4553
Yuyi He <yh383@cornell.edu>
2021-10-11 20:04:25 -0400

    change card

--- src/drawing.ml
+++ src/drawing.ml
@@ -22,7 +22,7 @@ let shuffle deck =
   deck |> List.map make_tuple |> List.sort tuple_compare
   |> List.map de_tuple
 
-let remaining_deck = ref (shuffle Card.card_deck)
+let remaining_deck = ref (shuffle card_deck)
 
 let rec deal_helper acc needed =
   if needed = 0 then acc
