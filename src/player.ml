a5d3b7c36bbabfa5b9a752b065af0048f7026ab3
Danyu <dh573@cornell.edu>
2021-10-16 22:17:32 -0400

    updated playe module

--- src/player.ml
+++ src/player.ml
@@ -1,22 +1,17 @@
-type player_hand = Card.card list
+type player = Card.card list
 
-type player = {
-  player_h : player_hand;
-  points : int;
-}
-
-let empty : player_hand = []
+let empty : player = []
 
 exception OutOfCards
 (**if a player tries play when they have no cards on hand, raise the
    OutOfCards exception*)
 
 (**[is_empty] represents if the player's hand [hand] is empty*)
-let is_empty (play : player) =
-  if play.player_h = empty then true else false
+let is_empty (play : player) = if play = [] then true else false
 
-(**[play_card] is the player's hand after playing a card*)
-let rec play_card (c : Card.card) (p_h : player_hand) =
+let build_player (clst : Card.card list) : player = clst
+
+let rec play_card (c : Card.card) (p_h : player) : player =
   match p_h with
   | h :: t -> if h = c then t else h :: play_card c t
   | empty -> raise OutOfCards
@@ -39,23 +34,16 @@ let rec insert_to_table
       if check_ind current ind then c :: table
       else h :: insert_to_table c t ind (current + 1)
 
-(**[card_back] is the player's hand after the player takes a card back
-   from the table*)
-let card_back (c : Card.card) (play : player) =
-  { player_h = c :: play.player_h; points = play.points }
+let card_back (c : Card.card) (play : player) : player = c :: play
 
 (**[tale_from_table] is a new combo in the table after the player takes
    back a card they played*)
-let rec take_from_table (c : Card.card) (table : Card.card list) =
+let rec take_from_table (c : Card.card) (table : Card.card list) :
+    player =
   match table with
   | [] -> raise Table.NoSuchCard
   | h :: t -> if h = c then t else h :: take_from_table c t
 
 (**[draw_to_player] is a player hand updated from drawing a card from
    the deck using [Drawing.draw]*)
-let draw_to_player (play : player) =
-  { player_h = Drawing.draw :: play.player_h; points = play.points }
-
-(**[add_pts_player] player with points added to them if they won a round*)
-let add_pts_player (play : player) =
-  { player_h = play.player_h; points = play.points + 1 }
+let draw_to_player (play : player) : player = Drawing.draw :: play
--- src/player.mli
+++ src/player.mli
@@ -1,10 +1,7 @@
-type player_hand
+type player
 (** The abstract type of values representing the player's hand. *)
 
-type player
-(** The abstract type of values representing the player's profile. *)
-
-val empty : player_hand
+val empty : player
 
 exception OutOfCards
 (**if a player tries play when they have no cards on hand, raise the
@@ -13,13 +10,16 @@ exception OutOfCards
 val is_empty : player -> bool
 (**[is_empty] represents if the player's hand [player] is empty*)
 
-val play_card : Card.card -> player_hand -> player_hand
+val build_player : Card.card list -> player
+(**[build_player] is a player's hand built from a list of cards*)
+
+val play_card : Card.card -> player -> player
 (**[play_card] is the player's hand after playing a card*)
+
+val card_back : Card.card -> player -> player
+(**[card_back] is the player's hand after the player takes a card back
+   from the table*)
 
 val draw_to_player : player -> player
 (**[draw_to_player] is a player profile updated from drawing a card from
    the deck*)
-
-val add_pts_player : player -> player
-(**[add_pts_player] player with points added to them, if they won a
-   round*)
